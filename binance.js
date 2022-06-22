9be6ca82e7438b31ae287178e7f47e5fae78ad84
MarsKiller <34848331+akgod@users.noreply.github.com>
2022-06-22 15:42:07 +0800

    Update binance.js

--- binance.js
+++ binance.js
@@ -3,11 +3,12 @@ const config = require("config");
 const rp = require('request-promise');
 const CryptoJS = require('crypto-js');  
 const moment = require("moment");
-const hostname = "https://fapi.binance.com";
+const ufuturehostname = "https://fapi.binance.com";
+const spothosdtname = "https://api.binance.com";
 
 class binance {
   async uFuture(){
-    let url = hostname + "/fapi/v1/ticker/24hr";  //返回U本位合约24h价格变动情况
+    let url = ufuturehostname + "/fapi/v1/ticker/24hr";  //返回U本位合约24h价格变动情况
     let options = {
       url: url,
       method: "get",
@@ -22,8 +23,78 @@ class binance {
    let bearNum = 0;
    let up5PercendNum =0;
    let down5PercendNum =0;
+   let maxBull = "BTCUSDT";
+   let maxBullPriceChangePercent = 0.0;
+   let maxBear = "BTCUSDT";
+   let maxBearPriceChangePercent = 0.0;
+   for(let i=0;i<rpbody.length;i++){     
+      if(rpbody[i].symbol.indexOf("USDT") != -1 && rpbody[i].symbol.indexOf("_") == -1){
+          //console.log("**********************************************");
+          //console.log('"'+rpbody[i].symbol+'"'+',',rpbody[i].priceChangePercent); //,rpbody[i].priceChangePercent 
+          //console.log(maxBull,maxBullPriceChangePercent,maxBear,maxBearPriceChangePercent);
+          usdtpairNum++;
+          if(parseFloat(rpbody[i].priceChangePercent) > maxBullPriceChangePercent){
+            maxBull = rpbody[i].symbol;
+            maxBullPriceChangePercent = rpbody[i].priceChangePercent;
+          }
+          if(parseFloat(rpbody[i].priceChangePercent) < maxBearPriceChangePercent){
+            maxBear = rpbody[i].symbol;
+            maxBearPriceChangePercent = rpbody[i].priceChangePercent;
+          }
+
+          if(parseFloat(rpbody[i].priceChangePercent) >0){
+            bullNum++;
+            if(parseFloat(rpbody[i].priceChangePercent) > 5){
+              up5PercendNum++;
+            }
+          }else{
+            bearNum++;
+            if(parseFloat(rpbody[i].priceChangePercent) < -5){
+              down5PercendNum++;
+            }
+          }       
+      }
+   }
+   let bullPercent = (bullNum / usdtpairNum).toFixed(2);
+   let bearPercent = (bearNum / usdtpairNum).toFixed(2);
+   let up5Percend = (up5PercendNum / bullNum).toFixed(2);
+   let down5Percend = (down5PercendNum / bearNum).toFixed(2);
+
+   console.log(`\n\n`);
+   console.log("**********************************************");
+   console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}]`);
+   console.log("U本位合约数量=",usdtpairNum);
+   console.log("上涨数量=",bullNum,"上涨比例=",bullPercent);
+   console.log("下跌数量=",bearNum,"下跌比例=",bearPercent);
+   console.log("涨幅5%以上数量=",up5PercendNum,"比例=",up5Percend);
+   console.log("跌幅5%以上数量=",down5PercendNum,"比例=",down5Percend);
+   console.log("当前涨幅最大：",maxBull,"上涨幅度=",maxBullPriceChangePercent);
+   console.log("当前跌幅最大",maxBear,"下跌幅度=",maxBearPriceChangePercent);
+   console.log("**********************************************");
+
+  }  
+
+  async spot(){
+    let url = spothosdtname + "/api/v3/ticker/24hr";  //返回现货24h价格变动情况 ?symbol=BTCUSDT
+    let options = {
+      url: url,
+      method: "get",
+      json: true,
+      headers: { "Accept": "application/json"},
+    };
+   let rpbody = await rp(options);  
+   console.log("rpnbody" , rpbody );
+   console.log("rpnbody-num=" , rpbody.length);
+   let usdtpairNum = 0;
+   let bullNum = 0;
+   let bearNum = 0;
+   let up5PercendNum =0;
+   let down5PercendNum =0;
+   
    for(let i=0;i<rpbody.length;i++){     
      if(rpbody[i].symbol.indexOf("USDT") != -1){
+      console.log('"'+rpbody[i].symbol+'"'+',',rpbody[i].priceChangePercent );
+
        usdtpairNum++;
        if(rpbody[i].priceChangePercent >0){
          bullNum++;
@@ -46,11 +117,11 @@ class binance {
    console.log(`\n\n`);
    console.log("**********************************************");
    console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}]`);
-   console.log("U本位合约数量=",usdtpairNum);
+   console.log("USDT现货交易对数量=",usdtpairNum);
    console.log("上涨数量=",bullNum,"上涨比例=",bullPercent);
    console.log("下跌数量=",bearNum,"下跌比例=",bearPercent);
    console.log("涨幅5%以上数量=",up5PercendNum,"比例=",up5Percend);
-   console.log("跌幅5%以上数量=",down5PercendNum,"下跌比例=",down5Percend);
+   console.log("跌幅5%以上数量=",down5PercendNum,"比例=",down5Percend);
    console.log("**********************************************");
 
   }  
@@ -61,4 +132,5 @@ let ba = new binance();
 module.exports = ba;
 
 //ba.uFuture().then(console.log)
+//ba.spot();
 
